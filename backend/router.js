78a89b23820d6d1aaf2f2943411645a608108e8e
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-10-09 11:50:54 -0700

    CLEAN CODE BASE

--- backend/router.js
+++ backend/router.js
@@ -4,24 +4,23 @@ const express = require('express');
 const toyRoute = express.Router();
 const uuid = require('uuid/v4');
 
-// let Khali =  { _id: uuid(), name: 'Baby Khaleesi', favorite_toy: 'dumbells'};
-// let Ginger = { _id: uuid(), name: 'Ginger', favorite_toy: 'bananas' };
-// let Khal = { _id: uuid(), name: 'Khal Basil', favorite_toy: 'anything' };
-// let Rosie = { _id: uuid(), name: 'Rosie', favorite_toy: 'ball' };
-// let Demi = { _id: uuid(), name: 'Demi Dog', favorite_toy: 'rope' }
+let Khali =  { _id: uuid(), name: 'Baby Khaleesi', favorite_toy: 'dumbells'};
+let Ginger = { _id: uuid(), name: 'Ginger', favorite_toy: 'bananas' };
+let Khal = { _id: uuid(), name: 'Khal Basil', favorite_toy: 'anything' };
+let Rosie = { _id: uuid(), name: 'Rosie', favorite_toy: 'ball' };
+let Demi = { _id: uuid(), name: 'Demi Dog', favorite_toy: 'rope' };
 
-// // data object
-// let toy = [Khali, Ginger, Khal, Rosie, Demi];
+let toy = [Khali, Ginger, Khal, Rosie, Demi];
 
-let pets = {
-  Khali: { _id: uuid(), name: 'Baby Khaleesi', favorite_toy: 'dumbells'},
-  Ginger: { _id: uuid(), name: 'Ginger', favorite_toy: 'bananas' },
-  Khal: { _id: uuid(), name: 'Khal Basil', favorite_toy: 'anything' },
-  Rosie: { _id: uuid(), name: 'Rosie', favorite_toy: 'ball' },
-  Demi: { _id: uuid(), name: 'Demi Dog', favorite_toy: 'rope' }
-};
+// let pets = {
+//   Khali: { _id: uuid(), name: 'Baby Khaleesi', favorite_toy: 'dumbells'},
+//   Ginger: { _id: uuid(), name: 'Ginger', favorite_toy: 'bananas' },
+//   Khal: { _id: uuid(), name: 'Khal Basil', favorite_toy: 'anything' },
+//   Rosie: { _id: uuid(), name: 'Rosie', favorite_toy: 'ball' },
+//   Demi: { _id: uuid(), name: 'Demi Dog', favorite_toy: 'rope' }
+// };
 
-let toy = Object.keys(pets);
+// let toy = Object.keys(pets);
 
 
 toyRoute.get('/toys', (request, response) =>  {
